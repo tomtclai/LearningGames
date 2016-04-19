c67fee2e7b4a49d4201ab03136e34274c6b61f03
Tom Lai <tomtclai@gmail.com>
2016-04-19 09:49:46 -0700

    Add Lab 2

---
+++ HTL-Lab2/src/HTLFunctionalAPI/Lab2.java
@@ -0,0 +1,17 @@
+package HTLFunctionalAPI;
+
+/**
+ * @author Tom Lai
+ * @author Jeen Cherdchusilp
+ */
+public class Lab2 extends HTLFunctionalAPI
+{
+	public void buildWorld()
+	{
+		drawMedicWizard(3,5);
+		drawMedicWizard(8,10);
+		drawSpeedyWizard(3,6);
+		drawSpeedyWizard(10,10);
+	}
+	
+}
--- HTL-Lab2/src/HTLFunctionalAPI/Main.java
+++ HTL-Lab2/src/HTLFunctionalAPI/Main.java
@@ -1,4 +1,4 @@
-
+package HTLFunctionalAPI;
 import Engine.GameWindow;
 @SuppressWarnings("serial")
 
