4e2671f648426703accb3092349693fdbb143046
Tom Lai <tomtclai@gmail.com>
2016-05-24 13:55:56 -0700

    fix lab5

--- HTL-Lab5/src/HTLFunctionalAPI/Lab5.java
+++ HTL-Lab5/src/HTLFunctionalAPI/Lab5.java
@@ -10,26 +10,29 @@ public class Lab5 extends HTLFunctionalAPI
 	public void updateGame() {
 		// in-game
 		if (mouseClicked()) {
-
+			int clickedRow = getClickedRow();
+			int clickedColumn = getClickedColumn();
+			
 			// if a Tower is selected, can it be moved to this Tile?
 			if (aTowerIsSelected()) {
-				moveTowerToClickedTile();
+				moveTowerTo(clickedColumn, clickedRow);
 			}
 			// otherwise, if there's a Tower on the tile, toggle selection
 			// of the tower
-			else if (clickedTileHasTower()) {
+			else if (tileHasTower(clickedRow,clickedColumn)) {
 
-				if (clickedTowerIsSelected()) {
+				if (towerIsSelected(clickedColumn, clickedRow)) {
 					unselectTower();
 				} else {
-					selectClickedTower();
+					selectTower(clickedColumn, clickedRow);
 				}
 			}
 			// otherwise, place a Tower
 			else {
 				// either speedy or medic, pick one
-				// addMedicTowerAtClickedTile();
-				addSpeedyTowerAtClickedTile();
+				// drawMedicWizard(clickedColumn, clickedRow);
+				drawSpeedyWizard(clickedColumn, clickedRow);
+				
 			}
 
 		}
